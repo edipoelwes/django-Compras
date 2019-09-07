3c31d5441c37a003041cdc3b6fbf9acd82400011
Edipo Elwes <edipoelwes2@gmail.com>
2019-09-07 11:28:47 -0300

    Versionando a VM

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/edipoelwes/Documentos/Desenv_web_back/Desenv.Backend/Lista_1/Questao_02/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
